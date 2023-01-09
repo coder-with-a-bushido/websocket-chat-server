0fe4b60926f506673ba9894e925e84364b4b815b
Karthikeyan S <karthikeyanssvk@gmail.com>
2023-01-09 08:30:19 +0530

    serve basic client

--- main.go
+++ main.go
@@ -13,10 +13,14 @@ func main() {
 	go room.handleRoom()
 	mux.HandleFunc("/ws", room.serveWs)
 
+	client := http.FileServer(http.Dir("./client"))
+	mux.Handle("/",client)
+
 	port, portFound := os.LookupEnv("PORT")
 	if !portFound {
 		port = "8001"
 	}
+	log.Println("Starting server...")
 	err := http.ListenAndServe(":"+port, mux)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
